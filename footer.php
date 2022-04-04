2778b0d24efbf49a0520bf2b082f564bb91668f6
LukasCJ <93259361+LukasCJ@users.noreply.github.com>
2022-04-04 10:22:00 +0200

    2022-04-04

---
+++ footer.php
@@ -0,0 +1,13 @@
+        <footer>
+            <ul>
+                <li class="nav_item"><a href="#"><p>About</p></a></li>
+                <li class="nav_item"><a href="#"><p>Contacts</p></a></li>
+            </ul>
+        </footer>
+
+    </div>
+    
+    <script src="js/script.js"></script>
+
+</body>
+</html>
