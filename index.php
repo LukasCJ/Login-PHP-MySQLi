c71ed2786bb1ea38c8819e11ad27e3749c265578
LukasCJ <93259361+LukasCJ@users.noreply.github.com>
2022-05-11 13:26:37 +0200

    h

--- index.php
+++ index.php
@@ -53,39 +53,4 @@
     </ul>
 </section>
 
-<section id="popular_reviews">
-    <div class="section_header"><h2>Popular Reviews</h2><p>Today</p></div>
-    <ul class="review_list">
-        <li class="review_container">
-            <div class="poster_container"><a href="#"><img src="img/metadata/poster.jpg" alt="poster"></a></div>
-            <div class="text_container">
-                <a href="#"><h4>Name</h4></a>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta eos tenetur possimus ipsa quae id reprehenderit, reiciendis maxime. Ipsum, illum?</p>
-            </div>
-        </li>
-        <li class="review_container">
-            <div class="poster_container"><a href="#"><img src="img/metadata/poster.jpg" alt="poster"></a></div>
-            <div class="text_container">
-                <a href="#"><h4>Name</h4></a>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum!</p>
-            </div>
-        </li>
-        <li class="review_container">
-            <div class="poster_container"><a href="#"><img src="img/metadata/poster_2.jpg" alt="poster"></a></div>
-            <div class="text_container">
-                <a href="#"><h4>Name</h4></a>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi iusto voluptates qui nisi, laborum doloremque provident minus earum natus laudantium aliquam vitae, aliquid cum officia blanditiis totam voluptatum consequuntur quisquam quasi. Dolorem magni, iusto ex totam esse quia error? Eveniet, excepturi, ipsa similique reprehenderit eum dolor debitis omnis alias error totam eius tenetur voluptatum sint! Saepe iste incidunt excepturi veritatis, voluptatum, tempora et accusantium consequuntur nesciunt quos laudantium odio enim voluptate porro aliquam, cumque esse.
-            </div>
-        </li>
-        <li class="review_container">
-            <div class="poster_container"><a href="#"><img src="img/metadata/poster.jpg" alt="poster"></a></div>
-            <div class="text_container">
-                <a href="#"><h4>Name</h4></a>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius est esse unde cupiditate in accusamus dolor laborum exercitationem ex odit fuga quisquam ab dolore tempore impedit saepe, iure delectus magnam, provident repudiandae deleniti soluta perspiciatis? Aperiam cumque, repudiandae quidem aspernatur porro consequatur minus quam vel nihil obcaecati reiciendis sapiente illo esse voluptates rerum repellat sint perspiciatis magnam nulla! In, inventore.</p>
-            </div>
-        </li>
-    </ul>
-    <div class="nav_item"><a href="#"><p>More Reviews</p></a></div>
-</section>
-
 <?php include_once 'footer.php' ?>
